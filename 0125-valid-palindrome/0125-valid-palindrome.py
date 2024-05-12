fb62eb0d0c8b83c0bd9496d5349295433bb9e455
Cecilia <153684178+cecechang@users.noreply.github.com>
2024-05-12 13:44:52 +0100

    Time: 24 ms (82.95%), Space: 13.8 MB (32.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def isPalindrome(self, s):
-        alphabet_list = [char for char in s if char.isalpha()]
+        alphabet_list = [char for char in s if char.isalpha() or char.isdigit()]
         new_s = ''.join(alphabet_list)
         if new_s.lower() == new_s[::-1].lower():
             return True
