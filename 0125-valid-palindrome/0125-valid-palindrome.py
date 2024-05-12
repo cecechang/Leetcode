394096f867e77b1329d66b98e25d4ac2317e67a0
Cecilia <153684178+cecechang@users.noreply.github.com>
2024-05-12 13:44:48 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        alphabet_list = [char for char in s if char.isalpha()]
+        new_s = ''.join(alphabet_list)
+        if new_s.lower() == new_s[::-1].lower():
+            return True
+        else:
+            return False
+        
