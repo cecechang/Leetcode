3d94e83c51cc2d95456ec980bc4181bb82a4f0b0
Cecilia <153684178+cecechang@users.noreply.github.com>
2024-06-18 21:18:10 +0100

    Time: 479 ms (9.51%), Space: 25.8 MB (81.22%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        num_dict = {}
+        for num in nums:
+            if num in num_dict:
+                return True
+            num_dict[num]=1
+        
