dfd8da010c5df7d662516e40a4ed4ec18d4d4de2
Cecilia <153684178+cecechang@users.noreply.github.com>
2024-07-06 13:47:27 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def passThePillow(self, n, time):
+        people_list = [x for x in range(1, n+1)]
+        people_list.extend(people_list[-2:0:-1])
+        while time > len(people_list):
+            time = time - len(people_list)
+        return people_list[time]
