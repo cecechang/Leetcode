e24cc8ba8270ef568dacdfaa6a361aa30caab177
Cecilia <153684178+cecechang@users.noreply.github.com>
2025-03-17 21:07:37 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,19 @@
 class Solution(object):
     def isPalindrome(self, s):
-        alphabet_list = [char for char in s if char.isalpha() or char.isdigit()]
-        new_s = ''.join(alphabet_list)
-        if new_s.lower() == new_s[::-1].lower():
-            return True
-        else:
-            return False
+        l, r = 0, len(s) - 1
+        while l < r: 
+            while l < r and not s[l].isalnum():
+                l += 1
+               
+            while l < r and (r >= 0 and not s[r].isalnum()):
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r += 1
+        return True
+            
+
+        
         
