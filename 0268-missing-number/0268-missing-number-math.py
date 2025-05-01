46a8ad09552fe876bfae4a42b651be7f7362ec83
Cecilia <153684178+cecechang@users.noreply.github.com>
2025-05-01 20:14:47 +0100

    Update 0268-missing-number-math.py

--- 0268-missing-number/0268-missing-number-math.py
+++ 0268-missing-number/0268-missing-number-math.py
@@ -1,7 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        expected_sum = (1 + n) * n / 2
+        expected_sum = (1 + n) * n // 2
         actual_sum = sum(nums)
-        return int(expected_sum - actual_sum)
+        return expected_sum - actual_sum
 
