f3bc7f68543c60acafa2206d58d6955f2225f424
Cecilia <153684178+cecechang@users.noreply.github.com>
2025-05-03 16:54:16 +0100

    Update 2206-divide-array-into-equal-pairs.py

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -2,12 +2,16 @@ class Solution:
     def divideArray(self, nums: List[int]) -> bool:
         store = {}
         for num in nums:
-            store[num] = store.get(num, 0) + 1
+            if num in store:
+                store[num] += 1
+            else:
+                store[num] = 1
 
         for count in store:
             if store[count] % 2 != 0:
                 return False
         return True
+                
                 
 
         
