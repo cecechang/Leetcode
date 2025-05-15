a398b49fcc8de1390df064b7eaeeca6f994fea91
Cecilia <153684178+cecechang@users.noreply.github.com>
2025-05-15 21:39:32 +0100

    Time: 0 ms (100%), Space: 18 MB (18.7%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        def is_prime(x: int) -> bool:
+            if x < 2:
+                return False
+            for i in range(2, int(x ** 0.5) + 1):
+                if x % i == 0:
+                    return False
+            return True
+
+        root = int(n ** 0.5)
+        return root * root == n and is_prime(root)
